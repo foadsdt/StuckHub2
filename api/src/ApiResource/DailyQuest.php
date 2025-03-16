303c6dde639f8313e5bc46f4dce974fbbe3c52cb
foad <foad.sdt@gmail.com>
2025-03-16 16:34:46 -0400

    Custom Api Resource :  - Build New Api Resource that doesn't have orm entity attribute

---
+++ api/src/ApiResource/DailyQuest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\ApiResource;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\State\DailyQuestStateProvider;
+
+#[ApiResource(
+    shortName: 'Quest',
+    provider: DailyQuestStateProvider::class
+)]
+class DailyQuest
+{
+
+    public int $id;
+
+    public function __construct(int $id)
+    {
+        $this->id = $id;
+    }
+
+}
---
+++ api/src/State/DailyQuestStateProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProviderInterface;
+use App\ApiResource\DailyQuest;
+
+class DailyQuestStateProvider implements ProviderInterface
+{
+    public function provide(Operation $operation, array $uriVariables = [], array $context = []): object|array|null
+    {
+        return [
+            new DailyQuest(4),
+            new DailyQuest(5)
+        ];
+    }
+}
